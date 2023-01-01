2f868166b0b9f6feb14e5f2bc55ebdaa409428da
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2023-01-01 11:46:18 +0700

    Update

---
+++ main/app/Http/Controllers/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        // validasi data
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        try {
+            $akun = $request->only('username', 'password');
+            if (Auth::attempt($akun)) {
+                return redirect()->route('adm.dashboardadmin');
+            } else {
+                return redirect()->route('login')->with(['error' => 'Wrong username or password!']);
+            }
+        } catch (QueryException $e) {
+            // return response()->json([
+            //     'message' => "Failed " . $e->errorInfo
+            // ]);
+            return redirect()->route('login')->with(['error' => $e->errorInfo]);
+        }
+    }
+}
---
+++ main/resources/views/admin/partials/menu.blade.php
@@ -0,0 +1,18 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item nav-category">Data</li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+                <i class="menu-icon mdi mdi-table"></i>
+                <span class="menu-title">Produk</span>
+            </a>
+        </li>
+        
+    </ul>
+</nav>
